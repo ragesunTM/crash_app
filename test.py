68a58ef71db29d11b3221a436e979e933b718d60
ragesunTM <potirage@gmail.com>
2020-01-03 00:07:45 +0900

    コメント挿入

--- test.py
+++ test.py
@@ -12,6 +12,8 @@ def main():
     target_api = URL+"/players/"
     playerTag = settings.PLAYERTAG
     url = target_api+playerTag
+
+    #ここがわからん
     headers = {
         "content-type": "application/json; charset=utf-8",
         "cache-control": "max-age=60",
@@ -19,10 +21,12 @@ def main():
     r = requests.get(url, headers=headers)
     data = r.json()
 
+    #命名要変更，内包表記もうちょい勉強
     l_nam = [d.get('iconUrls') for d in data['cards']]
     l_name = [d.get('medium') for d in l_nam]
     print(l_name)
 
+    #元々のやつ
     #print(json.dumps(data['cards'], indent=4))
 
 if __name__ == '__main__':
